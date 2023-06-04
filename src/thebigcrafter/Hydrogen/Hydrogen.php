1b47601592a9754c55d8abacac225241c6678349
nmtho <128484709+nmtho@users.noreply.github.com>
2023-06-05 00:37:48 +0700

    Setup Composer

---
+++ src/thebigcrafter/Hydrogen/Hydrogen.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of Hydrogen.
+ * (c) thebigcrafter <thebigcrafterteam@proton.me>
+ * This source file is subject to the Apache-2.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+declare(strict_types=1);
+
+namespace thebigcrafter\Hydrogen;
+
+use pocketmine\plugin\PluginBase;
+
+class Hydrogen extends PluginBase {}
