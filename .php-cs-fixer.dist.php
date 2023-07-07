36c9b3a3bbe31031bc1c324d96d721cef951086f
nmtho <128484709+nmtho@users.noreply.github.com>
2023-07-07 14:59:37 +0700

    Update email address

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 $header = <<<'EOF'
     This file is part of Hydrogen.
-    (c) thebigcrafter <thebigcrafterteam@proton.me>
+    (c) thebigcrafter <hello@thebigcrafter.xyz>
     This source file is subject to the Apache-2.0 license that is bundled
     with this source code in the file LICENSE.
     EOF;
